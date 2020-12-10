c6ae95abd2f0f8b6c59c9a1cb7ccec95062ee547
gizzieiam <59718290+gizzieiam@users.noreply.github.com>
2020-12-10 15:10:18 -0800

    inital commit

---
+++ dice.js
@@ -0,0 +1,43 @@
+/* When rolling two six-sided dice, each die will show a number between 1 and 6. When the numbers on the two dice are added together, they will sum to a number between 2 and 12.
+
+Use JavaScript to simulate 1000 rolls of a pair of dice. You will need to figure out how to generate random numbers in JavaScript, use your Google-Fu!
+
+For each possible roll (2 through 12), count its frequency. You should use an array to keep track of these counts.
+
+Initialize an array named count by filling it with zeros. Then, whenever the roll of the two dice adds up to, say, a seven, add one to the value in the 7th element of the counts array.
+
+ For example if the variable rollOfDice holds the current roll, you could increment the count with code like:
+*/
+// count[rollOfDice] = count[rollOfDice] + 1;
+
+// After the 1000 rolls are finished, show the final counts in an HTML page.
+
+/*
+Here's an example of the kind of output your program might produce:
+
+2: 32  
+3: 50  
+4: 76  
+5: 120  
+6: 151  
+7: 161  
+8: 125  
+9: 121  
+10: 89  
+11: 52  
+12: 23
+*/
+
+// https://www.w3schools.com/js/js_random.asp (help with getting random number)
+// https://www.w3schools.com/js/js_arrays.asp (help with array)
+
+ // TODO: make a function that shows the output random output of a die
+
+ // TODO: make a function that shows the output random output of two dice
+
+ // TODO: show the outcome of 1000 rolls
+
+//  TODO: make an array and store the outcome
+
+// TODO: display count in as a text
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+Readme.md is a very important file. this is were you tell others about your application. 
+
+when you create a repo of gitlab/github do not check the readme box. make your own using touch or adding a new file with your editor
+
+
+## RUBRIC
+
+Correctly roll 1 die
+1.0 pts
+
+Correctly roll a pair of dice
+1.0 pts
+
+Roll 1000 times
+1.0 pts
+
+Store the frequency counts in an array
+1.0 pts
+
+Display the counts in text
+1.0 pts
+
+Display the counts as a bar graph
+1.0 pts
